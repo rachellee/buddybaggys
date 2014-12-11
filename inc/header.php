36e2dedc64fbd988de12821dc467ead380772a34
Rachel Lee <rachellee03@gmail.com>
2014-12-10 17:32:57 -0800

    Changed contact form to mailto:
    more research to be done on smtp

--- inc/header.php
+++ inc/header.php
@@ -11,8 +11,19 @@
 	<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
 	<link rel="icon" href="/favicon.ico" type="image/x-icon">
 
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-57478908-1', 'auto');
+	  ga('send', 'pageview');
+	</script>
+
 	<script src="//use.typekit.net/qdq8opi.js"></script>
-<script>try{Typekit.load();}catch(e){}</script>
+	
+	<script>try{Typekit.load();}catch(e){}</script>
 
 	<!--[if lt IE 9]> <script src="//html5shiv.googlecode.com/svn/trunk/html5.js" type="text/javascript"></script><![endif]-->
 	
@@ -29,6 +40,6 @@
 			<span id="nav-link" class="home"><a href="index.php">Home</a></span>
 			<span id="nav-link" class="shop <?php if ($section == "shop") {echo "on";} ?>"><a href="shop.php">Shop</a></span>
 			<span id="nav-link" class="about <?php if ($section == "about") {echo "on";} ?>"><a href="about.php">About</a></span>
-			<span id="nav-link" class="contact <?php if ($section == "contact") {echo "on";} ?>"><a href="contact.php">Contact</a></span>
+			<span id="nav-link" class="contact <?php if ($section == "contact") {echo "on";} ?>"><a href="mailto:wesley@byham.com">Contact</a></span>
 		</div>
 	</header>
