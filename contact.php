c321fbf6a5ad29e88572c3d670339296568fb16f
Rachel Lee <rachellee03@gmail.com>
2015-06-21 22:40:49 -0700

    contact box

--- contact.php
+++ contact.php
@@ -1,3 +1,11 @@
-<div class="text-container">
-Questions, comments, inquires? Get in touch with Wesley: <a href="mailto:wesley@byham.com" target="_blank">wesley@byham.com</a> or 206-715-5461
-</div>
+<?php
+  $pageTitle = "About Doo Dog";
+  $section = "about";
+include('inc/header.php'); ?>
+
+  <div class="text-container">
+  Questions, comments, inquires? Get in touch with Wesley: <a href="mailto:wesley@byham.com" target="_blank">wesley@byham.com</a> or 206-715-5461
+  </div>
+
+
+<?php include('inc/footer.php'); ?>
