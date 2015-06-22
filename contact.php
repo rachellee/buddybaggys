9843e4790d60bb34c66ff112bc05f8b53e18d1f1
Rachel Lee <rachellee03@gmail.com>
2015-06-21 23:00:55 -0700

    contact box

---
+++ contact.php
@@ -0,0 +1,13 @@
+<?php
+  $pageTitle = "About Doo Dog";
+  $section = "about";
+include('inc/header.php'); ?>
+
+<div id="contact-container">
+  <div class="text-container"
+    <p>Questions, comments, inquires?<BR>
+    Get in touch with Wesley: <a href="mailto:wesley@byham.com" target="_blank">wesley@byham.com</a> or 206-715-5461<p>
+  </div>
+</div>
+
+<?php include('inc/footer.php'); ?>
