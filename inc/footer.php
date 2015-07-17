3c15496317eab9c00690c09d5060dd531778733b
Rachel Lee <rachellee03@gmail.com>
2015-07-16 22:33:37 -0700

    footer

--- inc/footer.php
+++ inc/footer.php
@@ -1,9 +1,6 @@
 </div>
 
 <footer>
-	<img src="images/pawprint2.png" class="pawprint-footer" alt="Paw Print">  <img src="images/pawprint2.png" class="pawprint-footer" alt="Paw Print">  <img src="images/pawprint2.png" class="pawprint-footer" alt="Paw Print"><BR><BR>
-	<div class="text-container">
-		<p>Made with love by<p>
-			<a href="http://www.rachellee.io" target="_blank">Rachel Lee</a>
-			</div>
+	<img src="images/pawprint2.png" class="pawprint-footer" alt="Paw Print">  <img src="images/pawprint2.png" class="pawprint-footer" alt="Paw Print">  <img src="images/pawprint2.png" class="pawprint-footer" alt="Paw Print"><BR>
+		<p>Made with love by<p><a href="http://www.rachellee.io" target="_blank">Rachel Lee</a>
 </footer>
