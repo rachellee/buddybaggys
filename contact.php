4723b938ea9560ca63dbf3e29953c145317b765e
Rachel Lee <rachellee03@gmail.com>
2015-08-21 21:26:15 -0700

    spacing for contact/wholesale copy

--- contact.php
+++ contact.php
@@ -6,7 +6,8 @@ include('inc/header.php'); ?>
 <div id="contact-container">
   <div class="text-container">
     <p>Questions, comments, inquires?<BR>
-    Get in touch with Wesley. Email her at <a href="mailto:wesley@doodogs.com" target="_blank">wesley@doodogs.com</a> or give her a call at 206-715-5461<p>
+    Get in touch with Wesley!<BR>
+    Email her at <a href="mailto:wesley@doodogs.com" target="_blank">wesley@doodogs.com</a> or give her a call at 206-715-5461<p>
   </div>
 </div>
 
--- wholesale.php
+++ wholesale.php
@@ -5,7 +5,8 @@ include('inc/header.php'); ?>
 
 <div id="wholesale-container">
   <div class="text-container">
-    <p>Interested in carrying Doo Dogs in your retail store? Let’s talk! Email Wesley at<a href="mailto:wesley@doodogs.com" target="_blank">wesley@doodogs.com</a> or give her a call at 206-715-5461<p>
+    <p>Interested in carrying Doo Dogs in your retail store? Let’s talk!<BR>
+    	Email Wesley at<a href="mailto:wesley@doodogs.com" target="_blank">wesley@doodogs.com</a> or give her a call at 206-715-5461<p>
   </div>
 </div>
 
