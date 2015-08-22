efe64ed7895343308689c6dd883c11c366cc1651
Rachel Lee <rachellee03@gmail.com>
2015-08-21 21:09:21 -0700

    footer, default font
    font to century gothic, not lato

--- inc/footer.php
+++ inc/footer.php
@@ -2,5 +2,5 @@
 
 <footer>
 	<img src="images/pawprint2.png" class="pawprint-footer" alt="Paw Print">  <img src="images/pawprint2.png" class="pawprint-footer" alt="Paw Print">  <img src="images/pawprint2.png" class="pawprint-footer" alt="Paw Print"><BR>
-		<p>Made with &heart; by<p><a href="http://www.rachellee.io" target="_blank">Rachel Lee</a>
+		<p>Made with &hearts; by<p><a href="http://www.rachellee.io" target="_blank">Rachel Lee</a>
 </footer>
